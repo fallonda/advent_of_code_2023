efb8aa8d6f90a6ec1511ee72cd10db7a602735c5
fallonda <david.j.fallon@gmail.com>
2023-12-11 23:19:16 +0000

    Completed day 07 part 2.

--- src/day_07/p2.R
+++ src/day_07/p2.R
@@ -15,14 +15,13 @@ read_input <- function(path) {
 }
 
 example <- read_input("./src/day_07/example.txt")
-
 example
 
 # Factors
 
 cards <- factor(
-    c("A","K","Q","J","T","9","8","7","6","5","4","3","2"),
-    levels = c("A","K","Q","J","T","9","8","7","6","5","4","3","2"),
+    c("A","K","Q","T","9","8","7","6","5","4","3","2","J"),
+    levels = c("A","K","Q","T","9","8","7","6","5","4","3","2","J"),
     ordered = TRUE
 )
 
@@ -50,11 +49,33 @@ hands <- factor(
 
 assign_hand <- function(hand) {
     split_hand <- str_split_1(hand, pattern = "")
-    freqs <- table(split_hand) |>
-        as.data.frame() |>
-        arrange(Freq) |>
-        mutate(Freq = as.numeric(Freq)) |>
-        pull(Freq)
+    if (grepl(x = hand, pattern = "J")) {
+        original_freqs <- table(split_hand) |>
+            as.data.frame() |> 
+            mutate(card = factor(split_hand, levels = cards, ordered = TRUE)) |>
+            arrange(Freq, desc(card))
+        # Get what J should be.
+        new_j <- original_freqs |>
+        filter(card != "J") |>
+        tail(n=1) |>
+        pull(card)
+        # Get the new hand with wildcard considered. 
+        freqs <- original_freqs |>
+            mutate(card = ifelse(card == "J", as.character(new_j), as.character(card))) |>
+            group_by(card) |>
+            summarise(Freq = sum(Freq)) |>
+            ungroup() |>
+            arrange(Freq) |>
+            mutate(Freq = as.numeric(Freq)) |>
+            pull(Freq)
+    } else {
+        freqs <- table(split_hand) |>
+            as.data.frame() |>
+            arrange(Freq) |>
+            mutate(Freq = as.numeric(Freq)) |>
+            pull(Freq)
+    }
+    
     ret_value <- case_when(
         identical(freqs, 5) ~ "Five of a kind",
         identical(freqs, c(1,4)) ~ "Four of a kind",
@@ -70,12 +91,19 @@ assign_hand <- function(hand) {
 
 # tests
 # assign_hand("AAAAA")
-# assign_hand("AAAAB")
-# assign_hand("AAACC")
-# assign_hand("AAABC")
-# assign_hand("AABBC")
-# assign_hand("AABCD")
-# assign_hand("ABCDE")
+# assign_hand("AAAAJ")
+# assign_hand("AAAAQ")
+# assign_hand("AAAJK")
+# assign_hand("AAAKK")
+# assign_hand("AAAKQ")
+# assign_hand("AAJKQ")
+# assign_hand("AAKKQ")
+# assign_hand("A3QQJ")
+# assign_hand("AAKQT")
+# assign_hand("AKQT9")
+# assign_hand("AKQTJ")
+# assign_hand("KTJJT")
+# assign_hand("J57J8")
 
 calc_winnings <- function(input) {
     calc_df <- input |>
@@ -108,13 +136,15 @@ calc_winnings <- function(input) {
         ) |>
         mutate(rank = row_number()) |>
         mutate(winnings = rank * bid)
-    return(sum(calc_df$winnings))
+    return(calc_df)
 }
 
 # Example
 calc_winnings(example)
+sum(calc_winnings(example)$winnings)
 
 # Full input
 full_input <- read_input("./src/day_07/full_input.txt")
-calc_winnings(full_input)
-
+results_df <- calc_winnings(full_input)
+sum(results_df$winnings)
+# View(results_df |> filter(grepl("J", hand)))
